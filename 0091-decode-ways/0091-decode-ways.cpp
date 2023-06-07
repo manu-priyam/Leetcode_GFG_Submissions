01f3c3d97a58fd6dd785bf3208004b1713eccb69
manu-priyam <121173628+manu-priyam@users.noreply.github.com>
2023-06-07 21:53:39 +0530

    Time: 5 ms (31.12%), Space: 6.1 MB (75.04%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n+1,0);
+        //empty suffix array, then no of ways to decode this array is simply 1
+        dp[n] = 1;
+
+        //Last character of the given array as the subproblem
+        dp[n-1] = s[n-1]=='0'?0:1;
+
+        for(int i = n-2;i>=0;i--){
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+            dp[i]=dp[i+1];
+            if(s[i] == '1' || (s[i] == '2' && s[i+1] >= '0' && s[i+1] <= '6')){
+                dp[i]+=dp[i+2];
+            }
+        }
+        return dp[0];
+    }
+};
